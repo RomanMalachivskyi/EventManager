4d28c58d2f8df05cc48c465e1dbd27b7f1fb09b2
rmalachivskyi <roman.malachivsky@gmail.com>
2020-02-13 14:57:21 +0200

    fix controller

--- src/main/java/com/event/manager/controller/CategoryController.java
+++ src/main/java/com/event/manager/controller/CategoryController.java
@@ -3,8 +3,10 @@ package com.event.manager.controller;
 import com.event.manager.repository.CategoryRepo;
 import com.event.manager.resource.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@Controller
 @RequestMapping(path = "/category")
 public class CategoryController {
 
@@ -16,7 +18,7 @@ public class CategoryController {
         return categoryRepo.findAll();
     }
 
-    @PostMapping(path="/add") // Map ONLY POST Requests
+    @PostMapping(path="/add")
     public @ResponseBody String addNewUser (@RequestParam String name) {
         Category n = new Category(name);
         categoryRepo.save(n);
